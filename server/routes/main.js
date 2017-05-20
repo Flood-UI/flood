aea845fde5f2794aa259f491a4152583b3e5af67
nle <nolooseends@users.noreply.github.com>
2017-05-20 15:46:31 +0200

    Flood @ hostname in the title
    Added hostname to the title of the page.

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,12 +3,14 @@ let config = require('../../config');
 let express = require('express');
 
 let router = express.Router();
+var os = require("os");
+var hostname = os.hostname();
 
 router.get('*', (req, res) => {
   res.render(
     'index',
     {
-      title: 'Flood',
+      title: `Flood @ ${hostname}`,
       baseURI: config.baseURI || '/',
       maxHistoryStates: config.maxHistoryStates || 30,
       pollInterval: config.pollInterval || 1000 * 5
