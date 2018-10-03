67d114924db2a2c2babf334018b6811eacc6e713
Ben Sampson <ben@pingalamedia.co.uk>
2018-10-03 14:31:49 +0100

    add about tab to settings modal

---
+++ client/src/javascript/components/modals/settings-modal/AboutTab.js
@@ -0,0 +1,30 @@
+import {injectIntl} from 'react-intl';
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import AboutMarkdownPath from '../../../../../../ABOUT.md';
+
+import SettingsTab from './SettingsTab';
+
+class AboutTab extends SettingsTab {
+  constructor(props) {
+    super(props)
+
+    this.state = { about: null }
+  }
+
+  componentWillMount() {
+    fetch(AboutMarkdownPath).then((response) => response.text()).then((text) => {
+      this.setState({ about: text })
+    })
+  }
+
+  render() {
+    return (
+      <div className="content">
+        <ReactMarkdown source={this.state.about} />
+      </div>
+    );
+  }
+}
+
+export default injectIntl(AboutTab);
