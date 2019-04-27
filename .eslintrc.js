72a20d9c1629461bc0ec1b9bc15763c6532d51a1
John Furrow <me@johnfurrow.com>
2019-04-27 16:05:53 -0700

    Update prettier position in eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  extends: ['prettier', 'airbnb', 'plugin:import/errors', 'plugin:import/warnings'],
+  extends: ['airbnb', 'plugin:import/errors', 'plugin:import/warnings', 'prettier'],
   parser: 'babel-eslint',
   plugins: ['flowtype', 'import'],
   rules: {
