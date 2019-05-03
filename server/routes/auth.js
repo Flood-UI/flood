bf125f0d1279f2fcecac85b8fe647a62967a6fa4
Hyeronimous Zawapete <hyeronimous.zawapete@gmail.com>
2019-05-03 08:19:06 +0200

    auth users route only for admin

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -139,6 +139,12 @@ router.get('/logout', (req, res) => {
   res.clearCookie('jwt').send();
 });
 
+router.use('/users', (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  }
+});
+
 router.get('/users', (req, res) => {
   Users.listUsers(ajaxUtil.getResponseFn(res));
 });
