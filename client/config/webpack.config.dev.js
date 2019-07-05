e8090d9c983b047670cc6e96c444e127f3af3dfa
John Furrow <hello@johnfurrow.com>
2019-07-05 13:52:49 -0700

    Explicitly use babelrc in SVG loader

--- client/config/webpack.config.dev.js
+++ client/config/webpack.config.dev.js
@@ -96,7 +96,7 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/env', '@babel/react'],
+              babelrc: true,
             },
           },
           {
--- client/config/webpack.config.prod.js
+++ client/config/webpack.config.prod.js
@@ -85,7 +85,7 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/env', '@babel/react'],
+              babelrc: true,
             },
           },
           {
