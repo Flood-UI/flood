9cbb845f742a53f655fcc4fed296af866978af9a
John Furrow <me@johnfurrow.com>
2019-09-20 23:04:42 -0700

    Refactors prettier formatter, formats CSS module type defs

--- client/src/javascript/components/general/Icon.scss.d.ts
+++ client/src/javascript/components/general/Icon.scss.d.ts
@@ -1,8 +1,7 @@
 declare const styles: {
-  readonly "icon": string;
-  readonly "sizeSmall": string;
-  readonly "sizeMedium": string;
-  readonly "fillCurrentColor": string;
+  readonly icon: string;
+  readonly sizeSmall: string;
+  readonly sizeMedium: string;
+  readonly fillCurrentColor: string;
 };
 export = styles;
-
